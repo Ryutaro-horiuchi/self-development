a129f0ea0ef784e9b60659613052f351eb6cc192
Ryutaro-horiuchi <64176388+Ryutaro-horiuchi@users.noreply.github.com>
2026-05-13 21:20:18 +0900

    典型1問

---
+++ algorithm/atcoder/競プロ典型90問/024.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+diff = 0
+
+for a, b in zip(A, B):
+    diff += abs(a-b)
+
+if diff <= k and (k - diff) % 2 == 0:
+    print("Yes")
+else:
+    print("No")
